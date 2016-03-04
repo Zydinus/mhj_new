ba6a9a5fed6010439e545eee215cafe56204bebd
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-03-04 21:00:58 +0700

    1st

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php include "include/include_pre.php" ?>
+<?php
+  requireSignin(false);
+  // requireLevel(100);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include 'include/include_head.php'; ?>
+
+    <title><?php echo $s_title; ?></title>
+
+  </head>
+  <body>
+    <?php include "include/include_body.php" ?>
+    <div class="container">
+      <h1>Hello, world!</h1>
+    </div>
+
+    <!--  nev bar -->
+    <?php include "nev_bar.php" ?>
+
+  </body>
+</html>
